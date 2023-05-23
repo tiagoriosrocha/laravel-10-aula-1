e5960ba2311bdd4549d30913daaf1c0faa0e3904
Tiago Rios da Rocha <tiagoriosrocha@gmail.com>
2023-05-23 20:51:22 -0300

    formatacao de espacamento

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StringsController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -27,5 +16,7 @@ Route::get('/olapessoa/{name?}', function($name='Estranho'){
 });
 
 Route::get('/paramaiusculo/{palavra}', [StringsController::class, 'paraMaiusculo']);
+
 Route::get('/paramaiusculoformatada/{palavra}', [StringsController::class, 'paraMaiusculoFormatada']);
+
 Route::get('/paracaractere/{palavra}', [StringsController::class, 'paraArray']);
